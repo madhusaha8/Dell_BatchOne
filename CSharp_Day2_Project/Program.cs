82d65500db4756f1ff4fb038835cd4cf55ad1d0d
Madhu_Saha <Madhu_Saha@dell.com>
2021-09-22 09:23:59 +0530

    Add project files.

---
+++ CSharp_Day2_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp_Day2_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
